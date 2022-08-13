c4ff494277feaf636da8b5db7682468a48090a10
PhamDuc <phamduc0405@gmai.com>
2022-08-13 10:38:15 +0700

    Tạo View Auto,Manual,.... Done

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using Asoft_Main_WPF.Core;
 using System;
-
+using System.Windows.Threading;
 
 namespace Asoft_Main_WPF.MVVM.ViewModel
 {
@@ -9,9 +9,31 @@ namespace Asoft_Main_WPF.MVVM.ViewModel
 
         public RelayCommand HomeViewCommand { get; set; }
         public RelayCommand ManualViewCommand { get; set; }
+        public RelayCommand DataViewCommand { get; set; }
+        public RelayCommand ConfigViewCommand { get; set; }
+        public RelayCommand TeachingViewCommand { get; set; }
+        public RelayCommand AlarmViewCommand { get; set; }
+        public RelayCommand MonitorViewCommand { get; set; }
 
         public HomeViewModel HomeVm { get; set; }
-        //public ManualViewModel ManualVm { get; set; }
+        public ManualViewModel ManualVm { get; set; }
+        public DataViewModel DataVm { get; set; }
+        public TeachingViewModel TeachingVm { get; set; }
+        public AlarmViewModel AlarmVm { get; set; }
+        public ConfigViewModel ConfigVm { get; set; }
+        public MonitorViewModel MonitorVm { get; set; }
+
+        private string _header;
+        public String Header
+        {
+            get { return _header; }
+            set
+            {
+                _header = value;
+                OnPropertyChanged();
+            }
+        }
+
         private object _currentview;
         public object Currentview
         {
@@ -22,21 +44,77 @@ namespace Asoft_Main_WPF.MVVM.ViewModel
                 OnPropertyChanged();
             }
         }
-
-
+        private DateTime _datetime;
+        public DateTime Datetime
+        {
+            get { return _datetime; }
+            set
+            {
+                _datetime = value;
+                OnPropertyChanged();
+            }
+        }
+        // public DateTime datetime { get; set; } = DateTime.Now;
         public MainViewModel()
         {
             HomeVm = new HomeViewModel();
-            //ManualVm = new ManualViewModel();
-            Currentview = HomeVm;
+            ManualVm = new ManualViewModel();
+            DataVm = new DataViewModel();
+            TeachingVm = new TeachingViewModel();
+            AlarmVm = new AlarmViewModel();
+            ConfigVm = new ConfigViewModel();
+            MonitorVm = new MonitorViewModel();
+            Currentview = ManualVm;
+            Header = "Manual";
+
+            #region Chuyển Màn Hình Child Auto,Manual
             HomeViewCommand = new RelayCommand(o =>
             {
                 Currentview = HomeVm;
+                Header = "Auto";
             });
-            //ManualViewCommand = new RelayCommand(o =>
-            //{
-            //    Currentview = ManualVm;
-            //});
+            ManualViewCommand = new RelayCommand(o =>
+            {
+                Currentview = ManualVm;
+                Header = "Manual";
+            });
+            DataViewCommand = new RelayCommand(o =>
+            {
+                Currentview = DataVm;
+                Header = "Data";
+            });
+            TeachingViewCommand = new RelayCommand(o =>
+            {
+                Currentview = TeachingVm;
+                Header = "Teaching";
+            });
+            ConfigViewCommand = new RelayCommand(o =>
+            {
+                Currentview = ConfigVm;
+                Header = "Config";
+            });
+            AlarmViewCommand = new RelayCommand(o =>
+            {
+                Currentview = AlarmVm;
+                Header = "Alarm";
+            });
+            MonitorViewCommand = new RelayCommand(o =>
+            {
+                Currentview = MonitorVm;
+                Header = "Monitor";
+            });
+            #endregion
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval =new TimeSpan(0,0,1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+           
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Datetime = DateTime.Now;
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,33 +18,62 @@ namespace Asoft_Main_WPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+        private int _selected;
+        public int Selected 
+        { 
+            get { return _selected; }
+            set 
+            { 
+                _selected = value;
+                switch (_selected)
+                {
+                    case 0: // Auto
+                        btn_Auto.Background = Brushes.DarkSlateBlue;
+                        btn_Manual.Background = btn_Teaching.Background = btn_Monitor.Background = btn_Data.Background = btn_Alarm.Background = btn_Config.Background = Brushes.Transparent;
+                        break;
+                    case 1: // Manual
+                        btn_Manual.Background = Brushes.DarkSlateBlue;
+                        btn_Auto.Background = btn_Teaching.Background = btn_Monitor.Background = btn_Data.Background = btn_Alarm.Background = btn_Config.Background = Brushes.Transparent;
+                        break;
+                        case 2: // Teaching
+                        btn_Teaching.Background = Brushes.DarkSlateBlue;
+                        btn_Auto.Background = btn_Manual.Background = btn_Monitor.Background = btn_Data.Background = btn_Alarm.Background = btn_Config.Background = Brushes.Transparent;
+                        break;
+                    case 3: // Monitor
+                        btn_Monitor.Background = Brushes.DarkSlateBlue;
+                        btn_Auto.Background = btn_Manual.Background = btn_Teaching.Background = btn_Data.Background = btn_Alarm.Background = btn_Config.Background = Brushes.Transparent;
+                        break;
+                    case 4: // Data
+                        btn_Data.Background = Brushes.DarkSlateBlue;
+                        btn_Auto.Background = btn_Manual.Background = btn_Teaching.Background = btn_Monitor.Background = btn_Alarm.Background = btn_Config.Background = Brushes.Transparent;
+                        break;
+                    case 5: // Alarm
+                        btn_Alarm.Background = Brushes.DarkSlateBlue;
+                        btn_Auto.Background = btn_Manual.Background = btn_Teaching.Background = btn_Monitor.Background = btn_Data.Background = btn_Config.Background = Brushes.Transparent;
+                        break;
+                    case 6: // Config
+                        btn_Config.Background = Brushes.DarkSlateBlue;
+                        btn_Auto.Background = btn_Manual.Background = btn_Teaching.Background = btn_Monitor.Background = btn_Data.Background = btn_Alarm.Background = Brushes.Transparent;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
-           // MessageBox.Show("DucPH");
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                DragMove();
-            }
-            if (e.ClickCount == 2)
-            {
-                if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
-                {
-                    Application.Current.MainWindow.WindowState = WindowState.Normal;
-                }
-                else
-                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
-            }
-        }
-
-        private void Border_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
+            // MessageBox.Show("DucPH");
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 DragMove();
@@ -64,5 +93,44 @@ namespace Asoft_Main_WPF
         {
             Application.Current.Shutdown();
         }
+        private void btnAuto_Click(object sender, RoutedEventArgs e)
+        {
+            Selected = 0;
+
+        }
+        private void btnManual_Click(object sender, RoutedEventArgs e)
+        {
+            Selected = 1;
+
+        }
+        private void btnTeaching_Click(object sender, RoutedEventArgs e)
+        {
+            Selected = 2;
+
+        }
+        private void btnMonitor_Click(object sender, RoutedEventArgs e)
+        {
+            Selected = 3;
+
+        }
+        private void btnData_Click(object sender, RoutedEventArgs e)
+        {
+            Selected = 4;
+
+        }
+        private void btnAlarm_Click(object sender, RoutedEventArgs e)
+        {
+            Selected = 5;
+
+        }
+        private void btnConfig_Click(object sender, RoutedEventArgs e)
+        {
+            Selected = 6;
+
+        }
+        
+        
+        
+        
     }
 }
